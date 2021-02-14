dc2500469a5617eff2b7d206b84d705891ac7f38
nlicitra <nickerous@gmail.com>
2021-02-14 09:36:27 -0500

    stuff

---
+++ src/babylon/scenes/ROOM005_temp.js
@@ -0,0 +1,174 @@
+import * as BABYLON from 'babylonjs';
+import {PBRCustomMaterial} from 'babylonjs-materials';
+// import {scaleLinear} from 'd3';
+import CaseTexture from '../../images/mix-art/rm004.jpg';
+import CDLabelTexture from '../../images/mix-labels/cd_template_RM004.png';
+
+export const caseTexture = CaseTexture;
+export const cdLabelTexture = CDLabelTexture;
+
+const main_vertexDefinitions = `
+vec3 mod289(vec3 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
+
+vec4 mod289(vec4 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
+
+vec4 permute(vec4 x) { return mod289(((x * 34.0) + 1.0) * x); }
+
+vec4 taylorInvSqrt(vec4 r) { return 1.79284291400159 - 0.85373472095314 * r; }
+
+float noise(vec3 v) {
+  const vec2 C = vec2(1.0 / 6.0, 1.0 / 3.0);
+  const vec4 D = vec4(0.0, 0.5, 1.0, 2.0);
+  vec3 i = floor(v + dot(v, C.yyy));
+  vec3 x0 = v - i + dot(i, C.xxx);
+  vec3 g = step(x0.yzx, x0.xyz);
+  vec3 l = 1.0 - g;
+  vec3 i1 = min(g.xyz, l.zxy);
+  vec3 i2 = max(g.xyz, l.zxy);
+
+  vec3 x1 = x0 - i1 + C.xxx;
+  vec3 x2 = x0 - i2 + C.yyy;
+  vec3 x3 = x0 - D.yyy;
+  i = mod289(i);
+  vec4 p = permute(permute(permute(i.z + vec4(0.0, i1.z, i2.z, 1.0)) + i.y + vec4(0.0, i1.y, i2.y, 1.0)) + i.x +
+                   vec4(0.0, i1.x, i2.x, 1.0));
+  float n_ = 0.142857142857;
+  vec3 ns = n_ * D.wyz - D.xzx;
+  vec4 j = p - 49.0 * floor(p * ns.z * ns.z);
+  vec4 x_ = floor(j * ns.z);
+  vec4 y_ = floor(j - 7.0 * x_);
+  vec4 x = x_ * ns.x + ns.yyyy;
+  vec4 y = y_ * ns.x + ns.yyyy;
+  vec4 h = 1.0 - abs(x) - abs(y);
+  vec4 b0 = vec4(x.xy, y.xy);
+  vec4 b1 = vec4(x.zw, y.zw);
+  vec4 s0 = floor(b0) * 2.0 + 1.0;
+  vec4 s1 = floor(b1) * 2.0 + 1.0;
+  vec4 sh = -step(h, vec4(0.0));
+  vec4 a0 = b0.xzyw + s0.xzyw * sh.xxyy;
+  vec4 a1 = b1.xzyw + s1.xzyw * sh.zzww;
+  vec3 p0 = vec3(a0.xy, h.x);
+  vec3 p1 = vec3(a0.zw, h.y);
+  vec3 p2 = vec3(a1.xy, h.z);
+  vec3 p3 = vec3(a1.zw, h.w);
+  vec4 norm = taylorInvSqrt(vec4(dot(p0, p0), dot(p1, p1), dot(p2, p2), dot(p3, p3)));
+  p0 *= norm.x;
+  p1 *= norm.y;
+  p2 *= norm.z;
+  p3 *= norm.w;
+
+  vec4 m = max(0.6 - vec4(dot(x0, x0), dot(x1, x1), dot(x2, x2), dot(x3, x3)), 0.0);
+  m = m * m;
+  return 42.0 * dot(m * m, vec4(dot(p0, x0), dot(p1, x1), dot(p2, x2), dot(p3, x3)));
+}
+
+vec3 distortFunct(vec3 transformed) {
+  float rv = -noise(transformed * radiusNoiseFrequency + iTime + 100.) * radiusVariationAmplitude;
+
+  return normalize(transformed) * (rv + radius);
+}
+
+vec3 orthogonal(vec3 v) { return normalize(abs(v.x) > abs(v.z) ? vec3(-v.y, v.x, 0.0) : vec3(0.0, -v.z, v.y)); }
+
+vec3 distortNormal(vec3 position, vec3 distortedPosition, vec3 normal) {
+  vec3 tangent1 = normalize(orthogonal(normal));
+  vec3 tangent2 = normalize(cross(normal, tangent1));
+  vec3 nearby1 = position + tangent1 * 0.013;
+  vec3 nearby2 = position + tangent2 * 0.013;
+  vec3 distorted1 = distortFunct(nearby1);
+  vec3 distorted2 = distortFunct(nearby2);
+  return normalize(cross(distorted1 - distortedPosition, distorted2 - distortedPosition));
+}
+`;
+
+const main_vertexBeforePositionUpdated = `
+float updateTime = iTime * .1;
+positionUpdated = distortFunct(positionUpdated);
+vec3 distortedNormal = distortNormal(position, positionUpdated, normalUpdated);
+normalUpdated = distortedNormal;
+`;
+
+// eslint-disable-next-line
+export const build = ({scene, audio} = {}) => {
+  const initTime = +new Date();
+  // const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), scene);
+  // light.intensity = 10;
+  scene.clearColor = BABYLON.Color3.FromHexString('#10161A');
+
+  // Make sure the CD is rendered in front of everything else i.e. top layer
+  const CD = scene.getTransformNodeByName('CDChassis');
+  CD.getChildren().forEach((child) => (child.renderingGroupId = 1)); // eslint-disable-line no-return-assign
+
+  const baseLight = new BABYLON.HemisphericLight('hemiLight', new BABYLON.Vector3(-1, 1, 0), scene);
+  baseLight.diffuse = new BABYLON.Color3(1, 1, 1);
+  baseLight.groundColor = new BABYLON.Color3(0.5, 0.5, 0.5);
+  baseLight.intensity = 0.25;
+
+  const dirLight = new BABYLON.DirectionalLight('dirLight', new BABYLON.Vector3(-1, -2, 1), scene);
+  dirLight.position = new BABYLON.Vector3(20, 40, -20);
+
+  const radius = 5;
+
+  const sphere = BABYLON.MeshBuilder.CreateSphere('sphere', {
+    diameter: radius * 2,
+    segments: 512,
+  });
+
+  const mainMaterial = new PBRCustomMaterial('sphereMaterial', scene);
+
+  mainMaterial.metallic = 0.1;
+  mainMaterial.roughness = 0.075;
+  mainMaterial.emissiveColor = new BABYLON.Color3.FromHexString('#293742');
+
+  sphere.material = mainMaterial;
+
+  //
+  // Shaders
+  //
+
+  mainMaterial.Vertex_Definitions(main_vertexDefinitions);
+  mainMaterial.Vertex_Before_PositionUpdated(main_vertexBeforePositionUpdated);
+
+  //
+  // Attributes
+  //
+
+  mainMaterial.AddUniform('iTime', 'float');
+  mainMaterial.AddUniform('radius', 'float');
+  mainMaterial.AddUniform('radiusVariationAmplitude', 'float');
+  mainMaterial.AddUniform('radiusNoiseFrequency', 'float');
+  mainMaterial.onBind = () => {
+    const time = (+new Date() - initTime) * 0.001;
+    mainMaterial.getEffect().setFloat('iTime', time);
+
+    mainMaterial.getEffect().setFloat('radius', radius);
+    mainMaterial.getEffect().setFloat('radiusVariationAmplitude', 1.2);
+    mainMaterial.getEffect().setFloat('radiusNoiseFrequency', 0.33);
+  };
+
+  // audio.makeBand('low', {
+  //   lowPass: 90,
+  //   highPass: 30,
+  // });
+  // audio.makeBand('high', {
+  //   highPass: 5000,
+  // });
+  // const lowScale = scaleLinear()
+  //   .domain([127, 130])
+  //   .range([10, 6]);
+  // lowScale.clamp(true);
+  // const highScale = scaleLinear()
+  //   .domain([0, 100000])
+  //   .range([0.01, 0.6]);
+  // highScale.clamp(true);
+  // const ampScale = scaleLinear()
+  //   .domain([127, 130])
+  //   .range([0, 1]);
+  // ampScale.clamp(true);
+
+  scene.registerBeforeRender(() => {
+    // const lowAmp = audio.getAverageAmplitude('low');
+    // const highEnergy = audio.getTotalEnergy('high');
+  });
+  return scene;
+};
--- src/babylon/scenes/templates/SeasonSix.js
+++ src/babylon/scenes/templates/SeasonSix.js
@@ -1,138 +1,161 @@
 import * as BABYLON from 'babylonjs';
-import {CustomMaterial} from 'babylonjs-materials';
 // import {scaleLinear} from 'd3';
-
-const {MeshBuilder, Mesh, Matrix} = BABYLON;
 
 // eslint-disable-next-line import/prefer-default-export
 export const builder = () => ({scene}) => {
-  const length = 10;
-  const segments = 500;
-  const radius = 0.05;
-  const spaceBetween = 0.5;
-  const rowItems = 21;
-  const totalItems = (rowItems + 1) * (rowItems + 1);
-
-  const blackColor = new BABYLON.Color3(16 / 255, 22 / 255, 26 / 255);
-  const whiteColor = new BABYLON.Color3(245 / 255, 248 / 255, 250 / 255);
-
-  const initTime = +new Date();
-
-  //
-  // Main scene ===============================
-  //
-
-  scene.clearColor = BABYLON.Color4.FromColor3(blackColor);
-
-  const camera = new BABYLON.ArcRotateCamera(
-    'camera',
-    -Math.PI / 2,
-    Math.PI / 2,
-    -0.0,
-    new BABYLON.Vector3(0, 0, 0),
-    scene,
-  );
-  // camera.attachControl(canvas, true);
-  camera.minZ = 0.1;
-  camera.fov = 1.15;
-
+  const CD = scene.getTransformNodeByName('CDChassis');
+  // Make sure the CD is rendered in front of everything else i.e. top layer
+  CD.getChildren().forEach((child) => (child.renderingGroupId = 1)); // eslint-disable-line no-return-assign
+  // const light = new BABYLON.PointLight('Omni', new BABYLON.Vector3(20, 20, 100), scene);
   const baseLight = new BABYLON.HemisphericLight('hemiLight', new BABYLON.Vector3(-1, 1, 0), scene);
   baseLight.diffuse = new BABYLON.Color3(1, 1, 1);
-  baseLight.groundColor = new BABYLON.Color3(1, 1, 1);
 
-  const mainMaterial = new CustomMaterial('tubeMaterial', scene);
-  mainMaterial.specularColor = new BABYLON.Color3(0.0, 0.0, 0.0);
-  mainMaterial.emissiveColor = new BABYLON.Color3(0.1, 0.1, 0.1);
-  mainMaterial.freeze();
+  let time = 0;
+  const rate = 0.01;
+  scene.registerBeforeRender(() => {
+    // const camera = scene.activeCamera;
+    // camera.alpha = time;
+    // light.position = camera.position;
+    time += scene.getAnimationRatio() * rate;
+  });
 
-  //
-  // Meshes
-  //
+  initShaders();
 
-  const contourMaterial = new CustomMaterial('tubeMaterial', scene);
-  contourMaterial.diffuseColor = whiteColor;
-  contourMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
-
-  const fillerMaterial = new CustomMaterial('tubeMaterial', scene);
-  fillerMaterial.diffuseColor = blackColor;
-  fillerMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
-
-  const path = [];
-  for (let i = 0; i < length; i += length / segments) {
-    path.push(new BABYLON.Vector3(0, 0, i - 1));
-  }
-  const meshParams = {
-    path,
-    tessellation: 32,
-    radius,
-    cap: BABYLON.Mesh.DOUBLESIDE,
+  const shaderMaterial = new BABYLON.ShaderMaterial(
+    'shader',
     scene,
-    updatable: false,
+    {
+      vertex: 'customEffect',
+      fragment: 'customEffect',
+    },
+    {
+      attributes: ['position', 'normal', 'uv'],
+      uniforms: ['iTime', 'iResolution'],
+    },
+  );
+
+  const postEffect = new BABYLON.PostProcess(
+    'customEffect',
+    'customEffect',
+    ['iTime', 'iResolution'],
+    [],
+    1,
+    scene.activeCamera,
+  );
+
+  postEffect.onApply = function(effect) {
+    effect.setVector2('iResolution', new BABYLON.Vector2(postEffect.width, postEffect.height));
+    effect.setFloat('iTime', time);
   };
-
-  const contourMesh = BABYLON.MeshBuilder.CreateTube('tube', {...meshParams, sideOrientation: BABYLON.Mesh.BACKSIDE});
-  contourMesh.material = contourMaterial;
-
-  const fillerMesh = MeshBuilder.CreateTube('tube', {...meshParams, sideOrientation: Mesh.FRONTSIDE});
-  fillerMesh.material = fillerMaterial;
-
-  const bufferMatrices = new Float32Array(16 * totalItems);
-  let idx = 0;
-  for (let y = -rowItems / 2; y <= rowItems / 2; y += 1) {
-    for (let x = -rowItems / 2; x <= rowItems / 2; x += 1) {
-      const matrix = Matrix.Translation(x * spaceBetween, y * spaceBetween, 0);
-      matrix.copyToArray(bufferMatrices, 16 * idx);
-      idx += 1;
-    }
-  }
-  contourMesh.thinInstanceSetBuffer('matrix', bufferMatrices, 16);
-  fillerMesh.thinInstanceSetBuffer('matrix', bufferMatrices, 16);
-
-  //
-  // Shaders
-  //
-
-  contourMaterial.Vertex_Before_PositionUpdated(`
-    positionUpdated = positionUpdated + normalUpdated * radius * (2.0 + positionUpdated.z * .025);
-
-    float t = iTime * speed;
-    float k = positionUpdated.z * factor;
-    positionUpdated.x += sin(k - t) * swingSize;
-    positionUpdated.y += cos(k - t) * swingSize;    
-  `);
-
-  fillerMaterial.Vertex_Before_PositionUpdated(`
-    float t = iTime * speed;
-    float k = positionUpdated.z * factor;
-    positionUpdated.x += sin(k - t) * swingSize;
-    positionUpdated.y += cos(k - t) * swingSize;    
-  `);
-
-  [fillerMaterial, contourMaterial].forEach((material) => {
-    material.AddUniform('iTime', 'float');
-    material.AddUniform('factor', 'float');
-    material.AddUniform('speed', 'float');
-    material.AddUniform('radius', 'float');
-    material.AddUniform('swingSize', 'float');
-
-    material.onBind = () => {
-      const time = (+new Date() - initTime) * 0.001;
-      material.getEffect().setFloat('iTime', time);
-      material.getEffect().setFloat('factor', 1.0);
-      material.getEffect().setFloat('speed', 5);
-      material.getEffect().setFloat('radius', radius);
-      material.getEffect().setFloat('swingSize', 0.15);
-    };
-  });
-
-  // FOV control
-
-  window.addEventListener('mousewheel', (e) => {
-    const sign = Math.sign(e.wheelDelta) * -1;
-
-    camera.fov += sign * 0.05;
-    camera.fov = Math.max(Math.min(camera.fov, 1.2), 0.25);
-  });
 
   return scene;
 };
+
+function initShaders() {
+  BABYLON.Effect.ShadersStore['customEffectFragmentShader'] = `
+  /*--------------------------------------------------------------------------------------
+  License CC0 - http://creativecommons.org/publicdomain/zero/1.0/
+  To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+  ----------------------------------------------------------------------------------------
+  ^ This means do ANYTHING YOU WANT with this code. Because we are programmers, not lawyers.
+  -Otavio Good
+  */
+
+  uniform float iTime;
+  uniform vec2 iResolution;
+  uniform sampler2D textureSampler;
+
+  varying vec3 vPosition;
+  varying vec2 vUV;
+
+  #define iChannel0 textureSampler
+
+  const int MAX_RAY_STEPS = 64;
+  const float RAY_STOP_TRESHOLD = 0.0001;
+  const int MENGER_ITERATIONS = 3;
+
+  float maxcomp(vec2 v) { return max(v.x, v.y); }
+
+  float sdCross(vec3 p) {
+    p = abs(p);
+    vec3 d = vec3(max(p.x, p.y),
+            max(p.y, p.z),
+            max(p.z, p.x));
+    return min(d.x, min(d.y, d.z)) - (1.0 / 3.0);
+  }
+
+  float sdCrossRep(vec3 p) {
+    vec3 q = mod(p + 1.0, 2.0) - 1.0;
+    return sdCross(q);
+  }
+
+  float sdCrossRepScale(vec3 p, float s) {
+    return sdCrossRep(p * s) / s;	
+  }
+
+  float scene(vec3 p) {
+    float scale = 1.0;
+    float dist = 0.0;
+    for (int i = 0; i < MENGER_ITERATIONS; i++) {
+      dist = max(dist, -sdCrossRepScale(p, scale));
+      scale *= 3.0;
+    }
+    return dist;
+  }
+
+  vec3 hsv2rgb(vec3 c)
+  {
+      vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+      vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+      return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+  }
+
+  vec4 colorize(float c) {
+    
+    float hue = mix(0.6, 1.15, min(c * 1.2 - 0.05, 1.0));
+    float sat = 1.0 - pow(c, 4.0);
+    float lum = c;
+    vec3 hsv = vec3(hue, sat, lum);
+    vec3 rgb = hsv2rgb(hsv);
+    return vec4(rgb, 1.0);	
+  }
+
+
+  void mainImage( out vec4 fragColor, in vec2 fragCoord )
+  {
+    vec2 screenPos = fragCoord.xy / iResolution.xy * 2.0 - 1.0;
+  //	vec2 mousePos = iMouse.xy / iResolution.xy * 2.0 - 1.0;
+    
+    //vec3 cameraPos = vec3(0.16 * sin(iTime), 0.16 * cos(iTime), iTime);
+    //vec3 cameraPos = vec3(0.0);
+    vec3 cameraPos = vec3(0.0, 0.0, iTime);
+    vec3 cameraDir = vec3(0.0, 0.0, 1.0);
+    vec3 cameraPlaneU = vec3(1.0, 0.0, 0.0);
+    vec3 cameraPlaneV = vec3(0.0, 1.0, 0.0) * (iResolution.y / iResolution.x);
+
+    vec3 rayPos = cameraPos;
+    vec3 rayDir = cameraDir + screenPos.x * cameraPlaneU + screenPos.y * cameraPlaneV;
+    
+    rayDir = normalize(rayDir);
+    
+    float dist = scene(rayPos);
+    int stepsTaken;
+    for (int i = 0; i < MAX_RAY_STEPS; i++) {
+      if (dist < RAY_STOP_TRESHOLD) {
+        continue;
+      }
+      rayPos += rayDir * dist;
+      dist = scene(rayPos);
+      stepsTaken = i;
+    }
+    
+    vec4 color = colorize(pow(float(stepsTaken) / float(MAX_RAY_STEPS), 0.9));
+    
+    fragColor = color;
+  }
+  void main() 
+  {
+      mainImage(gl_FragColor, vUV * iResolution.xy);
+  }
+  `;
+}
